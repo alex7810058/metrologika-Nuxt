620f190d547206ca72f3079cb3161f0fb57ebdf5
alex.7810058 <alex.7810058@gmail.com>
2026-04-29 14:39:04 +0700

    init

--- app/pages/users.vue
+++ app/pages/users.vue
@@ -1,12 +1,15 @@
+<template>
+  <Card>
+    <template #content>
+      <h1>Пользователи</h1>
+      <DataTable :value="users" scrollable scrollHeight="400px" tableStyle="min-width: 50rem">
+        <Column field="name" header="Имя"></Column>
+      </DataTable>
+    </template>
+  </Card>
+</template>
+
 <script setup lang="ts">
 // Авто-редирект если не залогинен (middleware)
 definePageMeta({ middleware: 'auth' });
 </script>
-
-<template>
-  <Card>
-    <template #content>
-      users
-    </template>
-  </Card>
-</template>
