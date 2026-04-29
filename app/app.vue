c37adacd2cd7ea519658fa249abe932669b7218b
alex.7810058 <alex.7810058@gmail.com>
2026-04-30 01:28:14 +0700

    init

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 // При инициализации приложения проверяем сессию и загружаем данные если авторизированы
+import type { User } from '~/types'
+import { useAuthData } from '~/composables/useAuthData'
+
 const { loggedIn } = useUserSession()
 
 // Если уже авторизованы (например, после перезагрузки страницы), загружаем все необходимые данные
